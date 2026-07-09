eccb30bd67111277071d3a763b454835983af7bd
Borislav Stanimirov <b@ibob.bg>
2026-07-09 10:35:01 +0300

    boilerplate

---
+++ benchmark.cpp
@@ -0,0 +1,8 @@
+#define PICOBENCH_IMPLEMENT
+#include <picobench/picobench.hpp>
+
+int main(int argc, char* argv[]) {
+    //picobench::runner r;
+    //r.parse_cmd_line(argc, argv);
+    //return r.run();
+}
